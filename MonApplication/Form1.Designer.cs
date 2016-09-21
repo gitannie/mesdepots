3d244dde0d17048ca97c152c2230072310fd52dd
Annie <gitannie@annie.pourriere@gmail.com>
2016-09-21 19:22:34 +0200

    Ajoutez des fichiers projet.

---
+++ MonApplication/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MonApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BT1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BT1
+            // 
+            this.BT1.Location = new System.Drawing.Point(37, 35);
+            this.BT1.Name = "BT1";
+            this.BT1.Size = new System.Drawing.Size(186, 67);
+            this.BT1.TabIndex = 0;
+            this.BT1.Text = "BBouton de base";
+            this.BT1.UseVisualStyleBackColor = true;
+            this.BT1.Click += new System.EventHandler(this.BT1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.BT1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BT1;
+    }
+}
+
---
+++ MonApplication/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BT1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bouton de base du chef de projet");
+        }
+    }
+}
